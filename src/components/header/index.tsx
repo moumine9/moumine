fec253b26a77196bef1fd2b440a4c2a8c147d676
Abdoul Moumine <23276714+moumine9@users.noreply.github.com>
2025-04-06 11:34:41 -0400

    Ajout d'un menu déroulant dans l'en-tête pour les paramètres de thème et de langue

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 
-const Header = () => (
+const Header = () => {
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
 
+  const toggleSettings = () => {
+    setIsSettingsOpen(!isSettingsOpen);
+  };
+
+  return (
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary" >
 		<div class="container-fluid">
 			<a class="navbar-brand" href="/">
@@ -10,14 +17,32 @@ const Header = () => (
 				<span class="navbar-toggler-icon" />
 			</button>
 			<div class="collapse navbar-collapse" id="navbarColor01">
-				<ul class="navbar-nav me-auto">
-{/* 					<li class="nav-item">
-						<a class={"nav-link"} href="/profile">Generate Resume</a>
-					</li> */}
+				<ul class="navbar-nav ms-auto">
+ 					<li class="nav-item dropdown">
+						<a class={"nav-link"} href="#" onClick={(e) => {
+                            e.preventDefault();
+                            toggleSettings();
+                        }}>
+                          <i class="fa fa-cog fa-duotone fa-2x" />
+                        </a>
+                        {isSettingsOpen && (
+                          <div class="dropdown-menu dropdown-menu-end position-absolute show" style={{ right: 0, top: '100%' }}>
+                            <a class="dropdown-item" href="#">
+                              <i class="fa fa-regular fa-moon fa-duotone me-2" />
+                              Thème
+                            </a>
+                            <a class="dropdown-item" href="#">
+                              <i class="fa fa-language fa-duotone me-2" />
+                              Langue
+                            </a>
+                          </div>
+                        )}
+					</li>
 				</ul>
 			</div>
 		</div>
-</nav >
-);
+    </nav>
+  );
+};
 
 export default Header;
